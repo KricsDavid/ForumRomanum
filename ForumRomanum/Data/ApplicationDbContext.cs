5ae63221a21fdbfbb4f6a01b1287e12aa9cf1991
kricsfalusi.david <kricsfalusi.david@BURG-1E05-09>
2024-11-18 09:01:37 +0100

    Add project files.

---
+++ ForumRomanum/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ForumRomanum.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumRomanum.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ForumRomanum/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ForumRomanum.Models
+{
+    public class User : IdentityUser
+    {
+        string? VezetekNev { get; set; }
+        string? KereszNev { get; set; }
+
+    }
+}
