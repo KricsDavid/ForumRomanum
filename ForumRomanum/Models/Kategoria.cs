449e9f564e5238e8714e6f69e29f61391b975d8a
KricsDavid <david@kricsfalusi.hu>
2024-11-21 11:22:34 +0100

    uj osztály

---
+++ ForumRomanum/Models/Kategoria.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumRomanum.Models
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreateDate { get; set; }
+
+       // public virtual ICollection<Topic> Topics { get; set; }
+    }
+}
