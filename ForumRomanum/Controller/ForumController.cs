53306ee4df6351cddfe1e9d577aecf8b91453259
KricsDavid <david@kricsfalusi.hu>
2024-12-04 14:17:46 +0100

    lingangu

---
+++ ForumRomanum/Controller/ForumController.cs
@@ -0,0 +1,30 @@
+﻿using ForumMotor_13BC_A.Data;
+using ForumMotor_13BC_A.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumRomanum.Controller
+{
+    public class ForumController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public ActionResult<List<Category>> GetCategories()
+        {
+            var categories = _context.Categories.ToList();
+
+
+            return Ok(categories);
+        }
+       /* public IActionResult Index()
+        {
+            return View();
+        }*/
+    }
+}
